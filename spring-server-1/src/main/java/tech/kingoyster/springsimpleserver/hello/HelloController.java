3306e410b529382b402de97090b9309d48ff8c76
vitran96 <tranuytrieuvi@gmail.com>
2023-09-23 00:59:36 +0700

    get remote addr

--- spring-server-1/src/main/java/tech/kingoyster/springsimpleserver/hello/HelloController.java
+++ spring-server-1/src/main/java/tech/kingoyster/springsimpleserver/hello/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
-    public String hello() {
-        return "Hello, world!";
+    public String hello(HttpServletRequest request) {
+        return request.getRemoteAddr();
     }
 }
