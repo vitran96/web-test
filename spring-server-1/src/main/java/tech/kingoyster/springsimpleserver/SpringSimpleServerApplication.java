4b86050938ff3cb61f90a2858e9269aedbe43fd4
vitran96 <tranuytrieuvi@gmail.com>
2023-09-22 23:42:39 +0700

    1st commit

---
+++ spring-server-1/src/main/java/tech/kingoyster/springsimpleserver/SpringSimpleServerApplication.java
@@ -0,0 +1,13 @@
+package tech.kingoyster.springsimpleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleServerApplication.class, args);
+	}
+
+}
---
+++ spring-server-1/src/test/java/tech/kingoyster/springsimpleserver/SpringSimpleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kingoyster.springsimpleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
