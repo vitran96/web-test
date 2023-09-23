22917e7effc0580fc747c39f068d9b463271e933
vitran96 <tranuytrieuvi@gmail.com>
2023-09-23 09:13:40 +0700

    feature - add cli client

---
+++ spring-cli-1/src/main/java/tech/kingoyster/springcli1/Springcli1Application.java
@@ -0,0 +1,13 @@
+package tech.kingoyster.springcli1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcli1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcli1Application.class, args);
+	}
+
+}
---
+++ spring-cli-1/src/test/java/tech/kingoyster/springcli1/Springcli1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kingoyster.springcli1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcli1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
