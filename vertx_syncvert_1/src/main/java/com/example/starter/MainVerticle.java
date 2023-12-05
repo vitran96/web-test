bf9dfa10b76cd27d33108e428cf510c19e24360b
vitran96 <tranuytrieuvi@gmail.com>
2023-12-05 09:49:48 +0700

    update

--- vertx_syncvert_1/src/main/java/com/example/starter/MainVerticle.java
+++ vertx_syncvert_1/src/main/java/com/example/starter/MainVerticle.java
@@ -3,6 +3,8 @@ package com.example.starter;
 import co.paralleluniverse.fibers.Suspendable;
 import io.vertx.core.Launcher;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.sync.Sync;
 import io.vertx.ext.sync.SyncVerticle;
 
 public class MainVerticle extends SyncVerticle {
@@ -14,17 +16,37 @@ public class MainVerticle extends SyncVerticle {
   @Suspendable
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+
+//    vertx.createHttpServer().requestHandler(req -> {
+//      req.response()
+//        .putHeader("content-type", "text/plain")
+//        .end("Hello from Vert.x!");
+//    }).listen(8888, http -> {
+//      if (http.succeeded()) {
+//        startPromise.complete();
+//        System.out.println("HTTP server started on port 8888");
+//      } else {
+//        startPromise.fail(http.cause());
+//      }
+//    });
+
+    try {
+      HttpServer httpServer = vertx.createHttpServer().requestHandler(req -> {
+        req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!");
+      });
+
+      System.out.println("Waiting for server to start...");
+      HttpServer server = Sync.awaitResult(h -> httpServer.listen(8888, h));
+
+      System.out.println("HTTP server started on port " + server.actualPort());
+
+      startPromise.complete();
+    } catch (Exception e) {
+      System.out.println("Failed to start server");
+//      e.printStackTrace();
+      startPromise.fail(e);
+    }
   }
 }
